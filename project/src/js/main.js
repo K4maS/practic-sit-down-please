f0734cc2d170ccb4aa156ce6c7aa4f241c9c6b80
K4maS <107948849+K4maS@users.noreply.github.com>
2023-03-11 20:28:10 +0500

    главная страница готова с пробелмой в слайдере и в блоке с грид

--- project/src/js/main.js
+++ project/src/js/main.js
@@ -8,69 +8,92 @@
     const formBtn = document.querySelector('.form__btn');
     const windowForm = document.querySelector('.main-block__form');
     const windowGalery = document.querySelector('.window__img-block');
+
+    const hederNav = document.querySelector('.header__nav');
+    const burgerBtn = document.querySelector('.header__burger');
+
     const windowInform = document.querySelector('.window__inform');
     const goodImgBtn = document.querySelectorAll('.good__img-btn');
     const inputs = document.querySelectorAll('.form__input');
 
+    let open = false;
+
     // green sock 
     const tlmenu = new TimelineMax({ paused: true });
     const tlwindow = new TimelineMax({ paused: true });
+
     document.addEventListener('DOMContentLoaded', function () {
-        // окна
-        windowCloseBtn.addEventListener('click', async function () {
-            await tlwindow.reverse();
-            windowBlock.classList.remove('window--active')
-        })
-        goodImgBtn.forEach(e => {
-            e.addEventListener('click', function () {
+        // бургер-меню
+        try {
+            burgerBtn.addEventListener('click', async function () {
+
+                if (!open) tlmenu.play();
+                else await tlmenu.reverse();
+                open = !open
+                burgerBtn.classList.toggle('burger--active');
+                hederNav.classList.toggle('nav--active');
+                document.body.classList.toggle('stop-scroll');
+            })
+        } catch { }
+        // окна   
+
+        try {
+            windowCloseBtn.addEventListener('click', async function () {
+                await tlwindow.reverse();
+                windowBlock.classList.remove('window--active')
+                document.body.classList.remove('stop-scroll');
+            })
+            goodImgBtn.forEach(e => {
+                e.addEventListener('click', function () {
+                    tlwindow.play();
+                    windowForm.classList.remove('form--active')
+                    windowGalery.classList.add('window__img-block--active')
+                    windowInform.classList.remove('window__inform--active')
+                    windowBlock.classList.add('window--active')
+                    document.body.classList.add('stop-scroll');
+                })
+            })
+
+            goodBtn.addEventListener('click', function () {
                 tlwindow.play();
-                windowForm.classList.remove('form--active')
-                windowGalery.classList.add('window__img-block--active')
+                windowForm.classList.add('form--active')
+                windowGalery.classList.remove('window__img-block--active')
                 windowInform.classList.remove('window__inform--active')
                 windowBlock.classList.add('window--active')
-
+                document.body.classList.add('stop-scroll');
             })
-        })
 
-        goodBtn.addEventListener('click', function () {
-            tlwindow.play();
-            windowForm.classList.add('form--active')
-            windowGalery.classList.remove('window__img-block--active')
-            windowInform.classList.remove('window__inform--active')
-            windowBlock.classList.add('window--active')
+            formBtn.addEventListener('click', async function () {
+                let valid = false;
 
-        })
+                inputs.forEach(e => {
+                    if (e.value) {
+                        valid = true;
+                    }
+                    if (e.classList.contains('just-validate-error-field')) {
+                        valid = false;
+                    }
+                })
+                if (valid) {
+                    windowForm.classList.remove('form--active')
+                    windowGalery.classList.remove('window__img-block--active')
+                    windowInform.classList.add('window__inform--active')
 
-        formBtn.addEventListener('click', function () {
-            let valid = true;
-            inputs.forEach(e => {
-                if (e.classList.contains('just-validate-error-field')) {
-                    valid = false;
                 }
+
             })
-            if (valid) {
-                windowForm.classList.remove('form--active')
-                windowGalery.classList.remove('window__img-block--active')
-                windowInform.classList.add('window__inform--active')
-            }
+        }
+        catch { }
 
-        })
+
         // green sock 
-        // tlmenu
-        //     .to('.nav__list-mobile ', {
-        //         duration: 0.3,
-        //         y: 10,
-        //         opacity: 1
-        //     })
-        //     .to('.nav__list', {
-        //         duration: 0.4,
-        //         opacity: 1
-        //     })
-        //     .from('.menu__tel', {
-        //         duration: 0.5,
-        //         opacity: 0,
+        tlmenu
+            .to('.header__nav', {
+                duration: 0.3,
+                x: 30,
+                opacity: 1
+            })
 
-        //     });
 
         tlwindow
             .to('.window', {
